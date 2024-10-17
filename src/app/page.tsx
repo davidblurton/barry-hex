f1c7c3cbb7f9fae4540d08cbfc61907d36c99f18
David Blurton <david.blurton@gmail.com>
2024-10-17 17:25:03 +0000

    Initial hexes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,323 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+import React, { useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import { Shape } from "three";
+import { Line, MapControls, OrthographicCamera, Text } from "@react-three/drei";
+
+import * as THREE from "three";
+import { Chord, Midi, Range } from "tonal";
+import Piano from "@/components/Piano";
+
+const colors = {
+  YELLOW_400: "#facc15",
+  BLUE_400: "#60a5fa",
+  BLUE_500: "#3b82f6",
+};
+
+class Point {
+  constructor(public x: number, public y: number) {}
+}
+
+function pointy_hex_corner(center: Point, size: number, i: number) {
+  const angle_deg = 60 * i - 30;
+  const angle_rad = (Math.PI / 180) * angle_deg;
+
+  return new Point(
+    center.x + size * Math.cos(angle_rad),
+    center.y + size * Math.sin(angle_rad)
+  );
+}
+
+const size = 1;
+
+function toGridPoint(q: number, r: number) {
+  return new Point(
+    size * (Math.sqrt(3) * q + (Math.sqrt(3) / 2) * r),
+    size * (3 / 2) * r
+  );
+}
+
+const HexShape = ({
+  hex,
+  onClick,
+  selected,
+}: {
+  hex: Hex;
+  onClick: (hex: Hex) => void;
+  selected: boolean;
+}) => {
+  const [hover, setHover] = useState(false);
+  const center = toGridPoint(hex.q, hex.r);
+
+  const shape = React.useMemo(() => {
+    const hex = new Shape();
+    const start = pointy_hex_corner(center, size, 0);
+
+    hex.moveTo(start.x, start.y);
+
+    for (let i = 1; i < 6; i++) {
+      const p = pointy_hex_corner(center, size, i);
+      hex.lineTo(p.x, p.y);
+    }
+
+    hex.lineTo(start.x, start.y);
+    return hex;
+  }, [center]);
+
+  const chord = Chord.get([hex.root, hex.quality]);
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+    <>
+      <mesh
+        onPointerEnter={() => setHover(true)}
+        onPointerLeave={() => setHover(false)}
+        onClick={() => onClick(hex)}
+      >
+        <shapeGeometry args={[shape]} />
+        <meshBasicMaterial
+          color={
+            selected
+              ? colors.YELLOW_400
+              : hover
+              ? colors.BLUE_500
+              : colors.BLUE_400
+          }
         />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+      </mesh>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+      <Line
+        points={shape.getPoints()}
+        color="black"
+        lineWidth={3}
+        dashed={false}
+      />
+
+      <Text
+        position={[center.x, center.y, 0]}
+        fontSize={0.4}
+        color="black"
+        anchorX="center"
+        anchorY="middle"
+      >
+        {chord.tonic + chord.aliases[0]}
+      </Text>
+
+      <Text
+        position={[center.x, center.y - size / 2, 0]}
+        fontSize={0.2}
+        color="black"
+        anchorX="center"
+        anchorY="middle"
+      >
+        {`${hex.q}, ${hex.r}`}
+      </Text>
+    </>
+  );
+};
+
+type Hex = {
+  q: number;
+  r: number;
+  root: string;
+  quality: string;
+  offset?: number;
+};
+
+const hexes: Hex[] = [
+  {
+    q: 0,
+    r: 0,
+    root: "Eb4",
+    quality: "7",
+    offset: 1,
+  },
+  {
+    q: 1,
+    r: 0,
+    root: "C4",
+    quality: "7",
+    offset: 1,
+  },
+  {
+    q: 2,
+    r: 0,
+    root: "A3",
+    quality: "7",
+    offset: 1,
+  },
+  {
+    q: 3,
+    r: 0,
+    root: "F#3",
+    quality: "7",
+    offset: 1,
+  },
+  {
+    q: 0,
+    r: -1,
+    root: "F#4",
+    quality: "6",
+  },
+  {
+    q: 1,
+    r: -1,
+    root: "Eb4",
+    quality: "6",
+  },
+  {
+    q: 2,
+    r: -1,
+    root: "C4",
+    quality: "6",
+  },
+  {
+    q: 3,
+    r: -1,
+    root: "A3",
+    quality: "6",
+  },
+  {
+    q: 0,
+    r: -2,
+    root: "F#4",
+    quality: "m6",
+  },
+  {
+    q: 1,
+    r: -2,
+    root: "Eb4",
+    quality: "m6",
+  },
+  {
+    q: 2,
+    r: -2,
+    root: "C4",
+    quality: "m6",
+  },
+  {
+    q: 3,
+    r: -2,
+    root: "A3",
+    quality: "m6",
+  },
+];
+
+// Range.numeric([0, 4]).forEach((q) => {
+//   Range.numeric([0, -4]).forEach((r) => {
+//     hexes.push(new Hex(q, r));
+//   });
+// });
+
+const store = {
+  target: new THREE.Vector3(0, 0, 5),
+};
+
+function Scene({
+  selected,
+  setSelected,
+}: {
+  selected?: Hex;
+  setSelected: (hex: Hex) => void;
+}) {
+  // useFrame((state, delta) => {
+  // https://stackoverflow.com/questions/75562296/how-do-you-animate-the-camera-with-react-three-fiber
+  // state.camera.position.x = THREE.MathUtils.damp(
+  //   state.camera.position.x,
+  //   store.target.x,
+  //   4,
+  //   delta
+  // );
+  // state.camera.position.y = THREE.MathUtils.damp(
+  //   state.camera.position.y,
+  //   store.target.y,
+  //   4,
+  //   delta
+  // );
+  // state.camera.position.z = THREE.MathUtils.damp(
+  //   state.camera.position.z,
+  //   store.target.z,
+  //   4,
+  //   delta
+  // );
+  // state.camera.lookAt(state.camera.position);
+  // });
+
+  function onClick(hex: Hex) {
+    setSelected(hex);
+
+    const gridPoint = toGridPoint(hex.q, hex.r);
+
+    const target = new THREE.Vector3(gridPoint.x, gridPoint.y, 5);
+    store.target = target;
+  }
+
+  return (
+    <>
+      {hexes.map((hex) => (
+        <HexShape
+          key={`${hex.q},${hex.r}`}
+          hex={hex}
+          onClick={onClick}
+          selected={selected?.q === hex.q && selected?.r === hex.r}
+        />
+      ))}
+    </>
+  );
+}
+
+function getNotesForHex(hex: Hex) {
+  return Range.numeric([0, 3])
+    .map((x) => x + hex.q)
+    .map((x) => x + (hex.offset !== undefined ? hex.offset : 0))
+    .map((x) => x + 1)
+    .map((x) => x + 1)
+    .map(Chord.steps(hex.quality, hex.root));
+}
+
+export default function CanvasPage() {
+  const initialHex = hexes[6];
+  const initialPoint = toGridPoint(initialHex.q, initialHex.r);
+
+  const [selected, setSelected] = useState<Hex | undefined>(initialHex);
+
+  return (
+    <>
+      <Canvas>
+        <color attach="background" args={["#f5efe6"]} />
+        <ambientLight intensity={1} />
+        <Scene selected={selected} setSelected={setSelected} />
+
+        <MapControls
+          makeDefault
+          zoomSpeed={1}
+          dampingFactor={1}
+          target={[initialPoint.x, initialPoint.y, 0]}
+          enableRotate={false}
+          minZoom={30}
+          maxZoom={100}
+        />
+
+        <OrthographicCamera
+          makeDefault
+          position={[initialPoint.x, initialPoint.y, 5]}
+          up={[0, 0, 1]}
+          zoom={70}
+          far={10000}
+        />
+        {/* <axesHelper args={[1]} /> */}
+      </Canvas>
+
+      <div className="absolute bottom-4 left-4 right-4 flex items-center justify-center">
+        {selected !== undefined && (
+          <Piano
+            selection={getNotesForHex(selected).map((x) => ({
+              noteNum: Midi.toMidi(x)!,
+              color: colors.YELLOW_400,
+            }))}
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        )}
+      </div>
+    </>
   );
 }
---
+++ src/components/Piano.tsx
@@ -0,0 +1,166 @@
+function range(length: number): number[] {
+  return Array.from({ length }, (_, i) => i);
+}
+
+type PianoOptions = {
+  start: number;
+  end: number;
+};
+
+type KeyData = {
+  index: number;
+  num: number;
+  type: "white" | "black";
+  coord: {
+    x: {
+      min: number;
+      max: number;
+    };
+    y: {
+      min: number;
+      max: number;
+    };
+  };
+  selected: string | undefined;
+};
+
+type PianoData = { keys: KeyData[]; height: number; width: number };
+
+function makePiano(selection: KeySelection, options: PianoOptions): PianoData {
+  const widthRatio = 0.625;
+  const heightRatio = 0.667;
+
+  const lowestNote = options.start;
+  const highestNote = options.end;
+
+  const selectionMap = new Map();
+
+  selection.forEach((x) => selectionMap.set(x.noteNum, x.color));
+
+  const numKeys = highestNote - lowestNote + 1;
+
+  const isWhite = (num: number) =>
+    ![1, 4, 6, 9, 11].includes((num + lowestNote + 3) % 12);
+
+  const numLowerWhites = (keyIndex: number) =>
+    range(keyIndex).reduce(
+      (acc: number, val: number) => (isWhite(val) ? acc + 1 : acc),
+      0
+    );
+
+  // Total # of white keys
+  const numWhiteKeys = numLowerWhites(numKeys);
+
+  // Dimension of keys
+  const height = 70;
+  const whiteWidth = height / 4;
+  const blackWidth = whiteWidth * widthRatio;
+  const width = numWhiteKeys * whiteWidth;
+
+  const keys = range(numKeys).map((i) => {
+    const num = i + lowestNote;
+    const offset = isWhite(i) ? 0 : -blackWidth / 2;
+    return {
+      index: i,
+      num,
+      type: isWhite(i) ? ("white" as const) : ("black" as const),
+      // Piano coordinate: x: lower to higher key, y: near to far
+      coord: {
+        x: {
+          min: whiteWidth * numLowerWhites(i) + offset,
+          max:
+            whiteWidth * numLowerWhites(i) +
+            offset +
+            (isWhite(i) ? whiteWidth : blackWidth),
+        },
+        y: {
+          min: isWhite(i) ? 0 : (1 - heightRatio) * height,
+          max: height,
+        },
+      },
+      selected: selectionMap.get(i + lowestNote),
+    };
+  });
+
+  return {
+    keys,
+    height,
+    width,
+  };
+}
+
+type KeySelection = {
+  noteNum: number;
+  color: string;
+}[];
+
+type PianoProps = {
+  onKeyPress?: (keyNum: number) => void;
+  selection: KeySelection;
+};
+
+export default function Piano(props: PianoProps) {
+  const middleC = 60;
+  const border = 2;
+  const opts = { start: 48, end: 84 + 12 };
+  const strokeColor = "#444";
+
+  const { selection, onKeyPress } = props;
+  const { height, width, keys } = makePiano(selection, opts);
+
+  const handleKeyPress = (keyNum: number) => {
+    if (onKeyPress !== undefined) {
+      onKeyPress(keyNum);
+    }
+  };
+
+  return (
+    <svg width={width + 2 * border} height={height + 2 * border}>
+      {keys
+        .filter((key) => key.type == "white")
+        .map((key) => (
+          <rect
+            key={"white" + key.num}
+            x={key.coord.x.min + border}
+            y={key.coord.y.min + border}
+            width={key.coord.x.max - key.coord.x.min}
+            height={key.coord.y.max - key.coord.y.min}
+            stroke={strokeColor}
+            fill={key.selected || "#fff"}
+            strokeWidth={border}
+            onClick={() => handleKeyPress(key.num)}
+          ></rect>
+        ))}
+
+      {keys
+        .filter((key) => key.type == "black")
+        .map((key) => (
+          <rect
+            key={"black" + key.num}
+            x={key.coord.x.min + border}
+            y={0 + border}
+            width={key.coord.x.max - key.coord.x.min}
+            height={key.coord.y.max - key.coord.y.min}
+            stroke={strokeColor}
+            fill={key.selected || strokeColor}
+            strokeWidth={border}
+            onClick={() => handleKeyPress(key.num)}
+          ></rect>
+        ))}
+
+      {keys
+        .filter((key) => key.num === middleC)
+        .map((key) => (
+          <circle
+            key={"dot" + key.num}
+            cx={
+              key.coord.x.min + border + (key.coord.x.max - key.coord.x.min) / 2
+            }
+            cy={key.coord.y.max - 8}
+            r={3}
+            fill={strokeColor}
+          ></circle>
+        ))}
+    </svg>
+  );
+}
